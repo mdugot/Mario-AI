d64bcb784fcaee61a2c34b919ff5b0372247cb10
Merlin Dugot <merlin.dugot@cross-compass.com>
2020-01-28 15:53:51 +0900

    * fix Ngram  * add logarithmique random

--- src/NGram.java
+++ src/NGram.java
@@ -11,11 +11,12 @@ public class NGram {
     TransitionTable table;
     int deepness;
 
-    private List<byte[]> parseLevelSlides(File file) throws IOException
+    private List<String> parseLevelSlides(File file) throws IOException
     {
         byte[] content = Files.readAllBytes(Paths.get(file.getPath()));
         int length = 0;
-        List<byte[]> slides = new ArrayList<byte[]>();
+        List<byte[]> bslides = new ArrayList<byte[]>();
+        List<String> slides = new ArrayList<String>();
         int y = 0;
         int x = 0;
         while (content[length] != '\n') {
@@ -29,17 +30,20 @@ public class NGram {
                 x = 0;
                 y += 1;
              } else {
-                if (slides.size() <= x) {
-                    slides.add(new byte[height]);
+                if (bslides.size() <= x) {
+                    bslides.add(new byte[height]);
                 }
-                slides.get(x)[y] = ascii;
+                bslides.get(x)[y] = ascii;
                 x += 1;
              }
+        }
+        for (byte[] bslide : bslides) {
+            slides.add(new String(bslide));
         }
         return slides;
     }
 
-    private void getCorpusFiles(File folder, List<List<byte[]>> levels) {
+    private void getCorpusFiles(File folder, List<List<String>> levels) {
         for (File file: folder.listFiles()) {
             if (file.isDirectory()) {
                 getCorpusFiles(file, levels);
@@ -55,33 +59,36 @@ public class NGram {
         }
     }
 
-    private TransitionTable buildTable(List<List<byte[]>> levels) {
-        TransitionTable table = new TransitionTable();
-        List<byte[]> slides = new ArrayList<byte[]>();
-        for (List<byte[]> level : levels) {
+    private TransitionTable buildTable(List<List<String>> levels) {
+        TransitionTable table = new TransitionTable(null);
+        List<String> slides = new ArrayList<String>();
+        for (List<String> level : levels) {
+            System.out.print("BUILD TABLE\n");
             slides.clear();
             for (int i = 0; i < deepness + 1; i++) {
                 slides.add(level.get(i));
             }
             table.addSlides(slides);
-            for (int i = deepness + 1; i < levels.size(); i++) {
+            for (int i = deepness + 1; i < level.size(); i++) {
                 slides.remove(0);
                 slides.add(level.get(i));
+                table.addSlides(slides);
+            }
+            while (slides.size() > 1) {
+                slides.remove(0);
                 table.addSlides(slides);
             }
         }
         return table;
     }
 
-    public List<byte[]> generate(int size) {
-        List<byte[]> level = new ArrayList<byte[]>();
+    public List<String> generate(int size) {
+        List<String> level = new ArrayList<String>();
         for (int i = 0; i < size; i++) {
             if (level.size() < deepness) {
-                level.add(table.chooseSlide(level));
-                System.out.print("-----\n");
+                level.add(table.chooseSlide(level, Math.min(deepness, size - i)));
             } else {
-                level.add(table.chooseSlide(level.subList(level.size() - deepness, level.size())));
-                System.out.print("-----\n");
+                level.add(table.chooseSlide(level.subList(level.size() - deepness, level.size()), Math.min(deepness, size - i)));
             }
         }
         return level;
@@ -91,13 +98,14 @@ public class NGram {
         this.deepness = deepness;
         File folder = new File(directory);
         System.out.println("load corpus files contents");
-        List<List<byte[]>> levels = new ArrayList<List<byte[]>>();
+        List<List<String>> levels = new ArrayList<List<String>>();
         getCorpusFiles(folder, levels);
         table = buildTable(levels);
     }
 
     public static void main(String[] args) {
-        NGram ngram = new NGram("levels/original", 2);
-        List<byte[]> level = ngram.generate(30);
+        NGram ngram = new NGram("levels/original", 4);
+        System.out.print("GENERATE\n");
+        List<String> level = ngram.generate(80);
     }
 }
--- src/TransitionTable.java
+++ src/TransitionTable.java
@@ -1,22 +1,31 @@
 import java.util.List;
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Collections;
+import java.util.stream.IntStream;
 
 public class TransitionTable {
-    HashMap<byte[], TransitionTable> nextSlides;
-    HashMap<byte[], Integer> counts;
-    Random random = new Random();
+    HashMap<String, TransitionTable> nextSlides;
+    HashMap<String, Integer> counts;
+    TransitionTable root;
 
-    public TransitionTable() {
-        nextSlides = new HashMap<byte[], TransitionTable>();
-        counts = new HashMap<byte[], Integer>();
+    public TransitionTable(TransitionTable root) {
+        nextSlides = new HashMap<String, TransitionTable>();
+        counts = new HashMap<String, Integer>();
+        if (root == null) {
+            this.root = this;
+        } else {
+            this.root = root;
+        }
     }
 
-    public void addSlides(List<byte[]> slides)
+    public void addSlides(List<String> slides)
     {
-        byte[] firstSlide = slides.get(0);
+        String firstSlide = slides.get(0);
         if (nextSlides.get(firstSlide) == null) {
-            nextSlides.put(firstSlide, new TransitionTable());
+            nextSlides.put(firstSlide, new TransitionTable(root));
             counts.put(firstSlide, 1);
         } else {
             counts.put(firstSlide, counts.get(firstSlide) + 1);
@@ -34,34 +43,80 @@ public class TransitionTable {
         return total;
     }
 
-    private byte[] getRandom() {
-        System.out.print("count ");
-        System.out.print(getTotalCount());
-        System.out.print("\n");
-        int count = 0;
-        int randn = random.nextInt(getTotalCount());
-
-        for (HashMap.Entry<byte[], Integer> entry : counts.entrySet()) {
-            count += entry.getValue();
-            if (randn < count) {
-                return entry.getKey();
-            }
+    private double getTotalLogCount() {
+        double total = 0;
+        for (int count : counts.values()) {
+            total += Math.log((double)count + 1);
         }
-        return new byte[0];
+        return total;
     }
 
-    public byte[] chooseSlide(List<byte[]> previousSlides) {
-        System.out.print("choose slide\n");
-        System.out.print("size : ");
-        System.out.print(previousSlides.size());
-        System.out.print("\n");
-        if (previousSlides == null || previousSlides.size() == 0) {
-            System.out.print("return random\n");
-            return getRandom();
+    private boolean canContinueUntil(int steps) {
+        if (steps <= 1) {
+            return true;
         }
-        byte[] fromSlide = previousSlides.remove(0);
-        System.out.print(new String(fromSlide));
-        System.out.print("\n");
-        return nextSlides.get(fromSlide).chooseSlide(previousSlides);
+        for (TransitionTable table : nextSlides.values()) {
+            if (table.canContinueUntil(steps - 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String getRandom(int deepness) {
+        List<Integer> randRange = new ArrayList<Integer>();
+        for (int n : IntStream.range(0, getTotalCount()).toArray()) {
+            randRange.add(n);
+        }
+        Collections.shuffle(randRange);
+        for (Integer randn : randRange) {
+            int count = 0;
+            for (HashMap.Entry<String, Integer> entry : counts.entrySet()) {
+                count += entry.getValue();
+                if (randn < count) {
+                    if (root.nextSlides.get(entry.getKey()).canContinueUntil(deepness)) {
+                        return entry.getKey();
+                    }
+                    break;
+                }
+            }
+        }
+        System.out.println("deepness : " + deepness);
+        System.out.println("counts : " + counts.size());
+        System.out.println("slide : " + (String)counts.keySet().toArray()[0]);
+        System.out.println("root size : " + root.nextSlides.size());
+        System.out.println("next counts : " + root.nextSlides.get(counts.keySet().toArray()[0]).counts.size());
+        return null;
+    }
+
+    private String getLogRandom(int deepness) {
+        Random rand = new Random();
+        while (counts.size() >= 1) {
+            double totalLogCount = getTotalLogCount();
+            double randn = rand.nextDouble() * totalLogCount;
+            double count = 0.0;
+            for (HashMap.Entry<String, Integer> entry : counts.entrySet()) {
+                count += Math.log((double)entry.getValue() + 1);
+                if (randn <= count) {
+                    if (root.nextSlides.get(entry.getKey()).canContinueUntil(deepness)) {
+                        return entry.getKey();
+                    }
+                    // counts.remove(entry.getKey());
+                    break;
+                }
+            }
+        }
+        return null;
+    }
+
+    public String chooseSlide(List<String> previousSlides, int deepness) {
+        if (previousSlides == null || previousSlides.size() == 0) {
+            String slide = getLogRandom(deepness);
+            System.out.print(slide);
+            System.out.print("\n");
+            return slide;
+        }
+        String fromSlide = previousSlides.remove(0);
+        return nextSlides.get(fromSlide).chooseSlide(previousSlides, deepness);
     }
 }
